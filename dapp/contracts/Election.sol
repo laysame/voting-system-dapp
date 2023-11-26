10adf14d9ef58402fe7964689aa146998521b841
Laysa Souza <karollinylaysa@gmail.com>
2023-11-26 22:46:23 +0000

    Implemented the Election contract and function to add candidates to an election. Added the tests on Election.js

--- dapp/contracts/Election.sol
+++ dapp/contracts/Election.sol
@@ -3,17 +3,26 @@
 pragma solidity >= 0.4.0 < 0.9.0;
 
 contract Election {
-     // Model a Candidate
+    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
-    // Read/write candidate
-    string public candidate;
 
-    // Constructor
+    // Read/write candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
     function election () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
