72d7a81283c30d7b61efa415fc3d14c208d70bcc
Laysa Souza <karollinylaysa@gmail.com>
2023-11-26 23:36:07 +0000

    Added the vote function to the contract and update tests

--- dapp/contracts/Election.sol
+++ dapp/contracts/Election.sol
@@ -10,9 +10,10 @@ contract Election {
         uint voteCount;
     }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     // Read/write candidates
     mapping(uint => Candidate) public candidates;
-
     // Store Candidates Count
     uint public candidatesCount;
 
@@ -24,5 +25,26 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
